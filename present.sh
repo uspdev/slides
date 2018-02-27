001d9aa5f1c355cc51c70d6fe177f92a05697943
André Luiz Girol <andregirol@gmail.com>
2018-02-27 19:02:14 -0300

    Fix num erro de variável que pintava tudo de azul

--- present.sh
+++ present.sh
@@ -26,7 +26,7 @@ fi
 if [ -d "$1" ]; then
 
     echo -e "${yellow} [ Iniciando ] ${nc}\n"
-    echo -e "Iniciando servidor interno. Para desligar,${lblue} use Ctrl + C ${lblue} \n\n\a"
+    echo -e "Iniciando servidor interno. Para desligar, ${lblue} use Ctrl + C ${nc} \n\n\a"
     
     node $prez $1 docs/$1 --theme=league --watch --serve
 
